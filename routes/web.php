7afdc13d1f33a766696ffcbad388b85e83b544e8
Arlan Ariandi Trislis <arlanariandi@gmail.com>
2022-12-15 15:17:03 +0700

    UPDATE: Add DashboardController Add middleware IsAdmin Add roles to users migretion Add AdminUserSeeder Update kernel add admin Update model user add roles Update fortify dissable register Update DataSeeder add admin user seeder Update navigation, profile show, welcome, and rotue web and others

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,12 +18,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+Route::middleware(['auth:sanctum', 'verified'])
+    ->name('dashboard.')
+    ->prefix('dashboard')
+    ->group(function () {
+        Route::get('/', [DashboardController::class, 'index'])->name('index');
+
+        Route::middleware(['admin'])->group(function () {
+            //
+        });
+    });
